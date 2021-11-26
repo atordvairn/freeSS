8631f7b48e1f5eb2b97ac4476a91fc5a40cb7cfa
volcareso <volcareso@gmail.com>
2021-11-26 21:44:56 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
 document.querySelector("#button").addEventListener("click", function(){
 try{
-   
-$.getJSON("https://throw-me-to.vercel.app/api/create/"+document.querySelector('#name').value+"="+document.querySelector('#url').value, function(json_data){
-   alert(JSON.stringify(json_data));
+
+$.ajax({
+type: "GET",
+url: "https://throw-me-to.vercel.app/api/create/okokkakaka=naice",
+cache: false,
+
+success: function(html){
+//$("#more").after(html);
+alert("Success!");
+}
 });
 
 }catch(e){alert(e)}
