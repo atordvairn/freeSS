1158882da65d7224ded0383e430fca407dffe058
volcareso <volcareso@gmail.com>
2021-11-26 20:54:56 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ try{
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-         document.querySelector("#url").value = xhttp.responseText;
+         alert(xhttp.responseText)
       }
   };
   xhttp.open("GET", `/create/${document.querySelector("#name").value}=${document.querySelector("#url").value}`, true);
