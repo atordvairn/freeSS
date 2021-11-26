26a020202e0cea9753ba3fce234a23793b91c4a5
volcareso <volcareso@gmail.com>
2021-11-26 17:53:35 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
 document.querySelector("#button").addEventListener("click", function(){
-  alert(0)
-});
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         document.querySelector("#url").value = xhttp.responseText;
+      }
+  };
+  xhttp.open("GET", `/create/${document.querySelector("#name").value}=${document.querySelector("#url").value}`, true);
+  xhttp.send();
+})
