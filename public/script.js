bd7c19076b29c49bb2f1f7bc1bfa523a697b0227
volcareso <volcareso@gmail.com>
2021-11-26 21:23:49 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 document.querySelector("#button").addEventListener("click", function(){
 try{
    
- window.location.replace("https://throw-me-to.vercel.app/api/create/"+document.querySelector('#name').value+"="+document.querySelector('#url').value+");
+ window.location.replace("https://throw-me-to.vercel.app/api/create/"+document.querySelector('#name').value+"="+document.querySelector('#url').value);
 
 }catch(e){alert(e)}
 })
