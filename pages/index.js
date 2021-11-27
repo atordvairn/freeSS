cf97dd144e52167f99005a8c24637f73489d13ac
volcareso <volcareso@gmail.com>
2021-11-27 09:40:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,30 +5,9 @@ import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { Input } from "@chakra-ui/react"
 import Script from 'next/script'
 import React from 'react'
-import $ from 'jquery'
 
 export default function Home() {
 React.useEffect(() => {
-    
-$("#button").on("click", function(){
-  var name = $("#name").val;
-  var url = $("#url").val;
-  var url_ = `https://throw-me-to.vercel.app/api/create/${name}=${url}`;
-  alert("ok till here");
-
-  $.ajax({
-    type: "GET",
-    url: url_,
-    cache: false,
-
-    success: function(request, status){
-      alert("Success!");
-    }
-    error: function (request, status, error) {
-      alert(error);
-      }
-  });
-})
 
   }, [])
 
