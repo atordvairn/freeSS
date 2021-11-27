93b9fcc2ce1dc49cf18ac0cf651ffe4265260bbe
volcareso <volcareso@gmail.com>
2021-11-27 08:50:07 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,21 +1,19 @@
-document.querySelector("#button").addEventListener("click", function(){
+$("#button").on("click", function(){
+  var name = $("#name").val;
+  var url = $("#url").val;
+  var url_ = `https://throw-me-to.vercel.app/api/create/${name}=${url}`;
+  alert("ok till here");
 
-var name = $("#name").val;
-var url = $("#url").val;
-var url_ = `https://throw-me-to.vercel.app/api/create/${name}=${url}`;
-alert("ok till here");
+  $.ajax({
+    type: "GET",
+    url: url_,
+    cache: false,
 
-$.ajax({
-  type: "GET",
-  url: url_,
-  cache: false,
-
-  success: function(request, status){
-    alert("Success!");
-  }
-  error: function (request, status, error) {
-    alert(error);
+    success: function(request, status){
+      alert("Success!");
     }
-});
-
+    error: function (request, status, error) {
+      alert(error);
+      }
+  });
 })
