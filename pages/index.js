14654fb5b01bef66a8c63dc7a53b4269adf6822f
volcareso <volcareso@gmail.com>
2021-11-27 09:34:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,27 @@ import React from 'react';
 
 export default function Home() {
 React.useEffect(() => {
-    alert("0")
+    
+$("#button").on("click", function(){
+  var name = $("#name").val;
+  var url = $("#url").val;
+  var url_ = `https://throw-me-to.vercel.app/api/create/${name}=${url}`;
+  alert("ok till here");
+
+  $.ajax({
+    type: "GET",
+    url: url_,
+    cache: false,
+
+    success: function(request, status){
+      alert("Success!");
+    }
+    error: function (request, status, error) {
+      alert(error);
+      }
+  });
+})
+
   }, [])
 
   return (
