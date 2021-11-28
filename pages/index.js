14deeb5915c66a24218278e3d6b7b42e9885d667
volcareso <volcareso@gmail.com>
2021-11-28 09:01:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,8 +44,10 @@ export default function Home() {
         <form onSubmit={registerUser}>
           
         <div className={styles.input_cont}>
+        <InputGroup />
          <InputLeftAddon children="https://throw-me.now.sh/to?q=" />
           <Input placeholder="my url name" required id="url" name="url" type="text" />
+        <InputGroup />
           <Input placeholder="enter your looong url" id="name" name="name" type="url" required/>
         </div>
         <div className={styles.button_doiIt}>
