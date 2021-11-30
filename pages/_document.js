1212916f8a345d9bf49c4212990a0f5f1a32121e
volcareso <volcareso@gmail.com>
2021-11-30 16:43:26 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -65,7 +64,6 @@ class MyDocument extends Document {
 
         </Head>
         <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
