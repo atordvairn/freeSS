abc3abb1fb7d176918822604b724abf7092735e5
atordvairn <atordvairn@gmail.com>
2022-01-13 19:58:41 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,22 +10,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="manifest.json">
+          <link rel="manifest" href="manifest.json" />
 
-          <meta name="mobile-web-app-capable" content="yes">
-          <meta name="apple-mobile-web-app-capable" content="yes">
-          <meta name="application-name" content="freeSS">
-          <meta name="apple-mobile-web-app-title" content="freeSS">
-          <meta name="theme-color" content="teal">
-          <meta name="msapplication-navbutton-color" content="teal">
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="application-name" content="freeSS" />
+          <meta name="apple-mobile-web-app-title" content="freeSS" />
+          <meta name="theme-color" content="teal" />
+          <meta name="msapplication-navbutton-color" content="teal" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="msapplication-starturl" content="/">
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-          <link rel="icon" type="image/png" sizes="192x192" href="/favicon.png">
-          <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/favicon.png">
-          <link rel="icon" type="image/png" sizes="512x512" href="/favicon.png">
-          <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="/favicon.png">
+          <link rel="icon" type="image/png" sizes="192x192" href="/favicon.png" />
+          <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/favicon.png" />
+          <link rel="icon" type="image/png" sizes="512x512" href="/favicon.png" />
+          <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="/favicon.png" />
 
           <script
             async
