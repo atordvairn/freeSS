e2155726b56519f6f44fae719b7b64ced6290095
atordvairn <atordvairn@gmail.com>
2022-01-22 10:32:04 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Button, Input, Link, Box, Heading } from "@chakra-ui/react"
+import { Button, Input, Link, Box, Heading, useColorMode } from "@chakra-ui/react"
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import Script from 'next/script'
 import React from 'react'
-
 export default function Home() {
+
+  const { colorMode } = useColorMode();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -26,8 +28,8 @@ export default function Home() {
            Simple tool to extract the screenshot of the given web page URL. made elegent and decent in design without the advertisements annoying you all the way.
         </Box>
         <Box boxShadow='xl' p='10' rounded='md'>
-           <a href="https://www.producthunt.com/posts/freess?utm_source=badge-review&utm_medium=badge&utm_souce=badge-freess#discussion-body" target="_blank" rel="noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/review.svg?post_id=327801&theme=light" alt="freeSS - a free and powerfull screenshot tool for the web. | Product Hunt" style={{width: "250px", height: "54px" }} /></a>
-           <a href="https://www.producthunt.com/posts/freess?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-freess" target="_blank" rel="noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=327801&theme=light" alt="freeSS - a free and powerfull screenshot tool for the web. | Product Hunt" style={{width: "250px", height: "54px" }} /></a>
+           <a href="https://www.producthunt.com/posts/freess?utm_source=badge-review&utm_medium=badge&utm_souce=badge-freess#discussion-body" target="_blank" rel="noreferrer"><img src={`https://api.producthunt.com/widgets/embed-image/v1/review.svg?post_id=327801&theme=${colorMode}`} alt="freeSS - a free and powerfull screenshot tool for the web. | Product Hunt" style={{width: "250px", height: "54px" }} /></a>
+           <a href="https://www.producthunt.com/posts/freess?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-freess" target="_blank" rel="noreferrer"><img src={`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=327801&theme=${colorMode}`} alt="freeSS - a free and powerfull screenshot tool for the web. | Product Hunt" style={{width: "250px", height: "54px" }} /></a>
         </Box>
       </main>
 
