8d37d998bb45e47eeea62bf9c9e2ea6253eaa1c0
atordvairn <atordvairn@gmail.com>
2022-01-22 06:03:42 +0000

    fixed giscus themes

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ import {
   useDisclosure,
   useColorMode,
 } from '@chakra-ui/react'
+import { useEffect } from 'react'
 
 export default function Home() {
 
@@ -117,6 +118,31 @@ export default function Home() {
   const { isOpen: isTwoOpen , onOpen: onModalTwoOpen, onClose: onModalTwoClose } = useDisclosure()
 
   const { colorMode, toggleColorMode } = useColorMode();
+
+
+  useEffect(() => {
+        const script = document.createElement('script')
+          script.src = 'https://giscus.app/client.js'
+          script.setAttribute('data-repo', "atordvairn/freeSS")
+          script.setAttribute('data-repo-id', "R_kgDOGq2aSQ")
+          script.setAttribute('data-category', "General")
+          script.setAttribute('data-category-id', "DIC_kwDOGq2aSc4CAquD")
+          script.setAttribute('data-mapping', "url")
+          script.setAttribute('data-reactions-enabled', "1")
+          script.setAttribute('data-emit-metadata', "0")
+          script.setAttribute('data-theme', colorMode)
+          script.setAttribute('crossorigin', 'anonymous')
+          script.async = true
+
+          const comments = document.querySelector(".giscus")
+          if (comments) comments.appendChild(script)
+
+          return () => {
+            const comments = document.getElementById("giscus")
+            if (comments) comments.innerHTML = ''
+          }
+  })
+
 
   return (
     <div className={styles.container}>
